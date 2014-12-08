a97024f882cd80ff317ca78cb0208da205e80f0d
TomWerner <tomwer3@gmail.com>
2014-12-08 12:50:01 -0600

    Fixed type in complex query parser

--- src/org/uiowa/cs2820/engine/queryparser/ComplexQueryParser.java
+++ src/org/uiowa/cs2820/engine/queryparser/ComplexQueryParser.java
@@ -3,7 +3,7 @@ package org.uiowa.cs2820.engine.queryparser;
 
 public class ComplexQueryParser extends QueryParser
 {    
-    public ComplexQueryParser(ComplexTokenizer tokenizer, SimpleTokenParser tokenParser)
+    public ComplexQueryParser(ComplexTokenizer tokenizer, ComplexTokenParser tokenParser)
     {
         super(tokenizer, tokenParser);
     }
