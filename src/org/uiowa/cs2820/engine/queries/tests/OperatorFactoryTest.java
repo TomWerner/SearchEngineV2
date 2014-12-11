e67c6d3eec855bde44ab7b2ecbdec7cdd000451e
cgrycki <christopher-grycki@uiowa.edu>
2014-12-11 16:46:16 -0600

    OperatorFactoryTest
    Added a junit test for OperatorFactory
    Currently only includes tests for getFieldOperator

---
+++ src/org/uiowa/cs2820/engine/queries/tests/OperatorFactoryTest.java
@@ -0,0 +1,61 @@
+package org.uiowa.cs2820.engine.queries.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.uiowa.cs2820.engine.queries.*;
+
+public class OperatorFactoryTest {
+
+	
+	@Test
+	public void getFieldOperatorReturnsFieldEquals() 
+	{
+		String operator = "Equals";
+		FieldOperator opClass = OperatorFactory.getFieldOperator(operator);
+		assertTrue(opClass instanceof FieldEquals);
+	}
+	@Test
+	public void getFieldOperatorReturnsFieldGreaterThan() 
+	{
+		String operator = ">";
+		FieldOperator opClass = OperatorFactory.getFieldOperator(operator);
+		assertTrue(opClass instanceof FieldGreaterThan);
+	}
+	@Test
+	public void getFieldOperatorReturnsFieldLessThan() 
+	{
+		String operator = "<";
+		FieldOperator opClass = OperatorFactory.getFieldOperator(operator);
+		assertTrue(opClass instanceof FieldLessThan);
+	}
+	@Test
+	public void getFieldOperatorReturnsFieldIncludes() 
+	{
+		String operator = "includes";
+		FieldOperator opClass = OperatorFactory.getFieldOperator(operator);
+		assertTrue(opClass instanceof FieldIncludes);
+	}
+	@Test
+	public void getFieldOperatorReturnsFieldPrefix() 
+	{
+		String operator = "starts with";
+		FieldOperator opClass = OperatorFactory.getFieldOperator(operator);
+		assertTrue(opClass instanceof FieldPrefix);
+	}
+	@Test
+	public void getFieldOperatorReturnsFieldPostfix() 
+	{
+		String operator = "ends with";
+		FieldOperator opClass = OperatorFactory.getFieldOperator(operator);
+		assertTrue(opClass instanceof FieldPostfix);
+	}
+	@Test
+	public void getFieldOperatorReturnsNull() 
+	{
+		String operator = "AnOperatorThatDoesNotExist";
+		FieldOperator opClass = OperatorFactory.getFieldOperator(operator);
+		assertTrue(opClass == null);
+	}
+
+}
