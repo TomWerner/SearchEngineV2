3a2fb09778d6f3112bf4a21627a5ba638182f7fe
cgrycki <christopher-grycki@uiowa.edu>
2014-12-11 15:59:57 -0600

    OperatorFactory getFieldOperator
    The getFieldOperator is now aware of
    greater than, less than, includes, and
    prefix.

--- src/org/uiowa/cs2820/engine/queries/OperatorFactory.java
+++ src/org/uiowa/cs2820/engine/queries/OperatorFactory.java
@@ -3,9 +3,17 @@ package org.uiowa.cs2820.engine.queries;
 public class OperatorFactory
 {
     public static FieldOperator getFieldOperator(String operator)
-    {
+    { 
         if (operator.equalsIgnoreCase("equals") || operator.equalsIgnoreCase("equal"))
             return new FieldEquals();
+        else if (operator.equalsIgnoreCase("greater than") || operator.equals(">"))
+        	return new FieldGreaterThan();
+        else if (operator.equalsIgnoreCase("lesser than") || operator.equals("<"))
+        	return new FieldLessThan();
+        else if (operator.equalsIgnoreCase("includes"))
+        	return new FieldIncludes();
+        else if (operator.equalsIgnoreCase("starts with") || operator.equalsIgnoreCase("prefix"))
+        	return new FieldPrefix();
         
         return null;
     }
