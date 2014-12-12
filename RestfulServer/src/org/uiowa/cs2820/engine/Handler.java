61c9aa2bfd1eb8558afe04940b8a01a390be1a41
Cale Bierman <cale-bierman@uiowa.edu>
2014-12-12 17:59:25 -0600

    forgot to add server stuff

--- RestfulServer/src/org/uiowa/cs2820/engine/Handler.java
+++ RestfulServer/src/org/uiowa/cs2820/engine/Handler.java
@@ -2,43 +2,34 @@ package org.uiowa.cs2820.engine;
 
 import java.util.List;
 import java.util.ArrayList;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 
 import org.glassfish.jersey.internal.util.collection.MultivaluedStringMap;
-import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONTokener;
-import org.codehaus.jettison.json.JSONException;
+import org.uiowa.cs2820.engine.queries.Queryable;
+import org.uiowa.cs2820.engine.queryparser.QueryParser;
 
 
-@Path("/")
+@Path("/searchengine")
 @Produces("text/html")
 public class Handler {
-    @PUT
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("Catenate")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public String Catenate(String req) {
-                List<String> params = new ArrayList<String>();
-                JSONTokener S = new JSONTokener(req);
-                JSONArray D = new JSONArray(); // empty by default
-                try { D = new JSONArray(S); }
-                catch (JSONException j) {};
-                for (int i=0; i<D.length(); i++) {
-                        String e = "";
-                        try { e = D.getString(i); }
-                        catch (JSONException j) {};
-                        params.add(e);
-                        // System.out.println("reponse position" + i + "equals" + e);
-                    }
-                D = new JSONArray();
-                for (int i=params.size()-1; i>=0; i--) D.put(params.get(i));
-                String eBody = D.toString();
-                return eBody;
-    }
-}
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("search")
+	public String performSearch(
+			@DefaultValue("") @QueryParam("query") String query
+			) {
+		QueryParser parser = new QueryParser(new ComplexTokenizer(), new ComplexTokenParser(), true);
+		Queryable parsedQuery = parser.parseQuery(query);
 
+	}
+
+}
