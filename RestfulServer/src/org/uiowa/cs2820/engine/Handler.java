847b15838ece74b00badfce029d388b6fbf875b1
TomWerner <tomwer3@gmail.com>
2014-12-12 18:07:10 -0600

    Hooked the database to the server
    Since I added the temporary database search I figured I'd hook it up to
    the webserver

--- RestfulServer/src/org/uiowa/cs2820/engine/Handler.java
+++ RestfulServer/src/org/uiowa/cs2820/engine/Handler.java
@@ -1,35 +1,46 @@
 package org.uiowa.cs2820.engine;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.io.File;
 
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
 
-import org.glassfish.jersey.internal.util.collection.MultivaluedStringMap;
+import org.uiowa.cs2820.engine.databases.AVLFieldDatabase;
+import org.uiowa.cs2820.engine.databases.FieldDatabase;
+import org.uiowa.cs2820.engine.databases.FieldFileNode;
+import org.uiowa.cs2820.engine.databases.IdentifierDatabase;
+import org.uiowa.cs2820.engine.databases.ValueFileNode;
+import org.uiowa.cs2820.engine.fileoperations.RAFile;
 import org.uiowa.cs2820.engine.queries.Queryable;
+import org.uiowa.cs2820.engine.queryparser.ComplexTokenParser;
+import org.uiowa.cs2820.engine.queryparser.ComplexTokenizer;
+import org.uiowa.cs2820.engine.queryparser.ParsingException;
 import org.uiowa.cs2820.engine.queryparser.QueryParser;
-
 
 @Path("/searchengine")
 @Produces("text/html")
-public class Handler {
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("search")
-	public String performSearch(
-			@DefaultValue("") @QueryParam("query") String query
-			) {
-		QueryParser parser = new QueryParser(new ComplexTokenizer(), new ComplexTokenParser(), true);
-		Queryable parsedQuery = parser.parseQuery(query);
+public class Handler
+{
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("search")
+    public String performSearch(@DefaultValue("") @QueryParam("query") String query) throws ParsingException
+    {
+        QueryParser parser = new QueryParser(new ComplexTokenizer(), new ComplexTokenParser(), true);
+        Queryable parsedQuery = parser.parseQuery(query);
+        Database database = getDatabase();
+        return database.matchQuery(parsedQuery).toString();
+    }
 
-	}
+    private Database getDatabase()
+    {
+        FieldDatabase fieldDB = new AVLFieldDatabase(new RAFile(new File("data/musicFieldDatabase.dat"), 128, FieldFileNode.MAX_SIZE));
+        IdentifierDatabase identDB = new IdentifierDatabase(new RAFile(new File("data/musicIdentifierDatabase.dat"), 128, ValueFileNode.MAX_SIZE));
+        return new IntegratedFileDatabase(fieldDB, identDB);
+    }
 
 }
